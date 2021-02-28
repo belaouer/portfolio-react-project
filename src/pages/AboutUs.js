220af5c98c452c9055ed4e7b92014c7c838f16f7
Belaouer Mohamed <mohamed.belaouer@univ-rouen.fr>
2021-02-28 21:17:18 +0100

    3. services section

--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -1,10 +1,12 @@
 import React from 'react';
-import AboutSection from '../components/AboutSection'
+import AboutSection from '../components/AboutSection';
+import ServicesSection from '../components/ServicesSection';
 
 const AboutUs = () => {
     return (
         <div className="aboutUs">
             <AboutSection/>
+            <ServicesSection/>
         </div>
     )
 }
