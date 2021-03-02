842e68291fafc181ab6a940887ca2bdc23c6dae9
Belaouer Mohamed <mohamed.belaouer@univ-rouen.fr>
2021-03-02 22:23:36 +0100

    12. animate the pages

--- src/App.js
+++ src/App.js
@@ -5,27 +5,31 @@ import OurWork from './pages/OurWork';
 import MovieDetail from './pages/MovieDetail';
 import GlobalStyle from './components/GlobalStyle';
 import Nav from './components/Nav';
-import { Route, Switch } from 'react-router-dom';
+import { Route, Switch, useLocation } from 'react-router-dom';
+import {AnimatePresence}  from 'framer-motion'
 
 function App() {
+  const location = useLocation()
   return (
     <div className="App">
       <GlobalStyle/>
       <Nav/>
-      <Switch>
-        <Route path="/" exact>
-          <AboutUS/>
-        </Route>
-        <Route path="/work" exact>
-          <OurWork/>
-        </Route>
-        <Route path="/work/:id">
-          <MovieDetail/>
-        </Route>
-        <Route path="contact">
-          <ContactUS/>
-        </Route>
-      </Switch>
+      <AnimatePresence exitBeforeEnter>
+        <Switch location={location} key={location.pathname}>
+          <Route path="/" exact>
+            <AboutUS/>
+          </Route>
+          <Route path="/work" exact>
+            <OurWork/>
+          </Route>
+          <Route path="/work/:id">
+            <MovieDetail/>
+          </Route>
+          <Route path="/contact">
+            <ContactUS/>
+          </Route>
+        </Switch>
+      </AnimatePresence>
     </div>
   );
 }
--- src/pages/ContactUs.js
+++ src/pages/ContactUs.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import {motion} from 'framer-motion';
+import {pageAnimation} from '../animation';
 
 const ContactUs = () => {
     return (
-        <div>
-            
-        </div>
+        <motion.div variants={pageAnimation} initial="hidden" animate="show" exit="exit">
+            <h2>Contact Us</h2>
+        </motion.div>
     )
 }
 
